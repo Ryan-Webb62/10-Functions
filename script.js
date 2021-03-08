66fd4ca39ea57295843639a9c6f13124f8af9801
Ryan <65420906+Ryan-Webb62@users.noreply.github.com>
2021-03-08 11:45:17 -0500

    Passing arguments: value vs reference

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 'use strict';
 
+// --------------------Passing Arguments: Value vs. Reference------------------
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  // passing a primative like flight just makes a copy it is not the same
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+newPassport(jonas);
+checkIn(flight, jonas);
+
+// -------------------- Default Params-----------------------
+/* 
 const bookings = [];
 
 const createBooking = function (
@@ -27,3 +59,4 @@ createBooking('LH123', 5);
 
 // If you want to skip a param you enter undefined to use default
 createBooking('LH123', undefined, 1000);
+ */
