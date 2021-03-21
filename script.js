ebf06b3170e565b23389e615e4038139553357dc
Ryan <65420906+Ryan-Webb62@users.noreply.github.com>
2021-03-21 14:33:57 -0400

    Closures Lecture pt2

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
 'use strict';
-//---------------------Closures--------------------------------------
 
+//--------------------Closures pt II --------------------------------
+
+// Example 2
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+// Re-assigning f function
+h();
+f();
+// console.dir(f);
+
+//---------------------Closures--------------------------------------
+/* 
 const secureBooking = function () {
   let passengerCount = 0;
 
@@ -15,7 +59,7 @@ const booker = secureBooking();
 booker();
 booker();
 booker();
-
+ */
 //---------------------Immediately Invoked Function Expressions ------
 /* 
 const runOnce = function () {
